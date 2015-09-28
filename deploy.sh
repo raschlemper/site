fd3e8264113df71200f2c3bd9947256fc390c1a8
raschlemper <raschlemper@gmail.com>
2015-09-28 06:59:10 -0300

    sites

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+rm -R /var/www/html/sites/css/
+cp -R css/  /var/www/html/sites/css/
+
+rm -R /var/www/html/sites/fonts/
+cp -R fonts/  /var/www/html/sites/fonts/
+
+rm -R /var/www/html/sites/js/
+cp -R js/  /var/www/html/sites/js/
+
+rm -R /var/www/html/sites/templates/
+cp -R templates/  /var/www/html/sites/templates/
+
+cp -R index.html  /var/www/html/sites/index.html
