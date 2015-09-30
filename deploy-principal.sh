ab8aa031cb4049dae7cf92111fe5f21e1e425977
rafael.schlemper <rafael.schlemper@gennera.com.br>
2015-09-30 09:58:50 -0300

    merda

---
+++ deploy-principal.sh
@@ -0,0 +1,2 @@
+rm -R /var/www/html/sites/principal/
+cp -R principal/  /var/www/html/sites/principal/
---
+++ principal/deploy.sh
@@ -0,0 +1,4 @@
+
+
+rm -R /var/www/html/sites/principal/
+cp -R ../principal/  /var/www/html/sites/principal/
