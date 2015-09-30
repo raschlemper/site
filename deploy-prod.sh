ea5198a56c2c008d9fc5c420d94e3b8dcd9469f1
raschlemper <raschlemper@gmail.com>
2015-09-29 23:07:50 -0300

    site principal

---
+++ deploy-prod.sh
@@ -0,0 +1,16 @@
+rm -R /home/rafael/Dropbox/Aplicativos/Pancake.io/teratec/css/
+cp -R css/  /home/rafael/Dropbox/Aplicativos/Pancake.io/teratec/css/
+
+rm -R /home/rafael/Dropbox/Aplicativos/Pancake.io/teratec/fonts/
+cp -R fonts/  /home/rafael/Dropbox/Aplicativos/Pancake.io/teratec/fonts/
+
+rm -R /home/rafael/Dropbox/Aplicativos/Pancake.io/teratec/js/
+cp -R js/  /home/rafael/Dropbox/Aplicativos/Pancake.io/teratec/js/
+
+rm -R /home/rafael/Dropbox/Aplicativos/Pancake.io/teratec/templates/
+cp -R templates/  /home/rafael/Dropbox/Aplicativos/Pancake.io/teratec/templates/
+
+rm -R /home/rafael/Dropbox/Aplicativos/Pancake.io/teratec/principal/
+cp -R principal/  /home/rafael/Dropbox/Aplicativos/Pancake.io/teratec/principal/
+
+cp -R index.html  /home/rafael/Dropbox/Aplicativos/Pancake.io/teratec/index.html
